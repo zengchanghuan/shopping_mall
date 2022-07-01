37480aba7892c8987f927a6ac41eb7e24fe1c238
zengchanghuan <zengchanghuan@gmail.com>
2022-07-01 10:54:08 +0800

    add go mod and go mvc file

---
+++ routers/adminRouters.go
@@ -0,0 +1,31 @@
+package routers
+
+import (
+	"shopping_mall/controllers/admin"
+	"shopping_mall/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminRoutersInit(r *gin.Engine) {
+	//middlewares.InitMiddleware中间件
+	adminRouters := r.Group("/admin", middlewares.InitMiddleware)
+	{
+		adminRouters.GET("/", admin.MainController{}.Index)
+		adminRouters.GET("/welcome", admin.MainController{}.Welcome)
+
+		adminRouters.GET("/login", admin.LoginController{}.Index)
+		adminRouters.POST("/doLogin", admin.LoginController{}.DoLogin)
+
+		adminRouters.GET("/manager", admin.ManagerController{}.Index)
+		adminRouters.GET("/manager/add", admin.ManagerController{}.Add)
+		adminRouters.GET("/manager/edit", admin.ManagerController{}.Edit)
+		adminRouters.GET("/manager/delete", admin.ManagerController{}.Delete)
+
+		adminRouters.GET("/focus", admin.FocusController{}.Index)
+		adminRouters.GET("/focus/add", admin.FocusController{}.Add)
+		adminRouters.GET("/focus/edit", admin.FocusController{}.Edit)
+		adminRouters.GET("/focus/delete", admin.FocusController{}.Delete)
+
+	}
+}
